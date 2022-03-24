bcc7ee76a826ef6617bcb32a20480dd0afad544f
ferney9210 <ferneybautistab@gmail.com>
2022-03-24 14:49:49 -0500

    actualizacion

--- js/script.js
+++ js/script.js
@@ -15,35 +15,5 @@ let rand = Math.random() * 7200;
   valor = (valor-parseInt(valor.toString().split(".")[0]))*360;
   ruleta.style.transform = "rotate("+rand+"deg)";
 
-  setTimeout(() => {
-   switch (true) {
-      case valor > 0 && valor <= 45:
-       alert('2 Estrellas');
-       break;
-      case valor > 45 && valor <= 90:
-       alert('5 piezas');
-       break;
-      case valor > 90 && valor <= 135:
-       alert('2 corazones');
-       break;
-      case valor > 135 && valor <= 180:
-       alert('2 Nigiri');
-       break;
-      case valor > 180 && valor <= 225:
-       alert('Handroll Mini');
-       break;
-      case valor > 225 && valor <= 270:
-       alert('Descortez');
-       break;
-      case valor > 270 && valor <= 315:
-       alert('Coca Cola 2 L');
-       break;
-      case valor > 315 && valor <= 360:
-       alert('2 enjoys');
-       break;
-   
-   }
-   
-}, 5000);
 }
 }
